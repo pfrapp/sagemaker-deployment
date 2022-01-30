fb8949f23f34f9aeb3fda89cf026e07911da343e
EC2 Default User <ec2-user@ip-172-16-75-84.ec2.internal>
2022-01-30 23:40:33 +0000

    Implemented data preprocessing and training

--- Project/train/train.py
+++ Project/train/train.py
@@ -67,9 +67,39 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     device       - Where the model and data should be loaded (gpu or cpu).
     """
     
-    # TODO: Paste the train() method developed in the notebook here.
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
 
-    pass
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+
+            # TODO (Done): Complete this train method to train the model provided.
+
+            # Note: Most of this code has been taken from
+            # the RNN exercise of chapter 3.
+            # Note that we do not decouple the hidden states
+            # from their history, as the forward function
+            # does not give us the hidden state and we are not
+            # supposed to change the model itself.
+            # It turns out it works nevertheless.
+            # We also do not perform gradient clipping.
+
+            # zero accumulated gradients
+            model.zero_grad()
+
+            # get the output from the model
+            output = model(batch_X)
+
+            # calculate the loss and perform backprop
+            loss = loss_fn(output.squeeze(), batch_y.float())
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
